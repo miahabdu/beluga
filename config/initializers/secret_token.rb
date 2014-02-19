abc5434267d4847f52f8d1bc4567eace407694d4
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2014-02-19 18:48:50 -0500

    master first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GastroChub::Application.config.secret_key_base = '7c23b106785647581ae51767bb6e9d07397cceb2158bc78ebcde61388a2f5f37e99b5e33e10acf6cee923c028cb86a67c9e5fd041f92696dcadc56f67f7c0092'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GastroChub::Application.config.session_store :cookie_store, key: '_gastro_chub_session'
