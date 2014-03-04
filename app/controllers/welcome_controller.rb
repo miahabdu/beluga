f2b2ca09bb9b7608c265261565df3d67c7312169
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2014-03-03 22:46:01 -0500

    master fix permissions and categories

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,30 +8,35 @@ class WelcomeController < ApplicationController
 
   def food
     @posts = Post.order('created_at DESC')
+    @featured = Post.featured.order('created_at DESC')
     @post_pages = Kaminari.paginate_array(Post.where(category: 'Food').order('created_at DESC')).page(params[:page]).per(15)
     @page_category = "Food"
   end
 
   def drinks
     @posts = Post.order('created_at DESC')
+    @featured = Post.featured.order('created_at DESC')
     @post_pages = Kaminari.paginate_array(Post.where(category: 'Drinks').order('created_at DESC')).page(params[:page]).per(15)
     @page_category = "Drinks"
   end
 
   def travel
     @page_category = "Travel"
+    @featured = Post.featured.order('created_at DESC')
     @posts = Post.order('created_at DESC')
     @post_pages = Kaminari.paginate_array(Post.where(category: 'Travel').order('created_at DESC')).page(params[:page]).per(15)
   end
 
   def tags
     @page_category = "##{params[:tag]}"
+    @featured = Post.featured.order('created_at DESC')
     @posts = Post.order('created_at DESC')
     @post_pages = Kaminari.paginate_array(Post.tagged_with(params[:tag]).order('created_at DESC')).page(params[:page]).per(15)
   end
 
   def search
     @page_category = "Search results for: #{params[:query]}"
+    @featured = Post.featured.order('created_at DESC')
     @posts = Post.order('created_at DESC')
     @post_pages = Kaminari.paginate_array(Post.text_search(params[:query]).order('created_at DESC')).page(params[:page]).per(15)
   end
