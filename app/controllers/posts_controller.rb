2390fb6e93ac16454f59081d5336f306f0654cf4
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2014-03-03 22:54:20 -0500

    master fix before filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, only: [:edit, :update, :destroy. :new, :create]
+  before_filter :authenticate_user!, only: [:edit, :update, :destroy, :new, :create]
   # GET /posts
   # GET /posts.json
   def index
