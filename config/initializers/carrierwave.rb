2ebdaf73d625515759a1d2e06e5a22805582de4a
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2014-03-05 15:44:32 -0500

    master remove aws creds

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIN4C2JKL7SMMH3EA',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_access_key_id      => '',                        # required
+    :aws_secret_access_key  => '',                        # required
   }
-  config.fog_directory  = 'gastrochub'                     # required
+  config.fog_directory  = ''                     # required
 end
